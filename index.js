b733ad7319e9ff1f660aab41ab0c208b3fe81c61
Rhiann Fleming <rhiannf04@gmail.com>
2025-01-13 18:57:49 -0800

    done

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-const server = require('./backend/server')
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, NavLink, Routes, Route } from 'react-router-dom'
+import AppClass from './components/AppClass'
+import AppFunctional from './components/AppFunctional'
+import './styles/reset.css'
+import './styles/styles.css'
 
-const PORT = process.env.PORT || 9000
+const container = document.getElementById('root')
+const root = createRoot(container)
 
-server.listen(PORT, () => {
-  console.log(`listening on ${PORT}`)
-})
+root.render(
+  <BrowserRouter>
+    <h1>Welcome to the GRID</h1>
+    <nav>
+      <NavLink to="/">Functional</NavLink>
+      <NavLink to="/class-based">Class-Based</NavLink>
+    </nav>
+    <Routes>
+      <Route path="/" element={<AppFunctional className="functional" />} />
+      <Route path="class-based" element={<AppClass className="class-based" />} />
+    </Routes>
+  </BrowserRouter>
+)
